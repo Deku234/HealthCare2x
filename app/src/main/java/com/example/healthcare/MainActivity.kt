253af9f48259784227c6395fe7b23802e2c9b0f3
Debarun Acharya <debarunacharya2002@gmail.com>
2023-04-04 15:44:46 +0530

    Update & Bug Fixes

--- app/src/main/java/com/example/healthcare/MainActivity.kt
+++ app/src/main/java/com/example/healthcare/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.healthcare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myButton: Button = findViewById(R.id.buttonLogIn)
+        myButton.setOnClickListener {
+
+            val intent = Intent(this, Homeactivity::class.java)
+            startActivity(intent)
+        }
+        val myTextView = findViewById<TextView>(R.id.textViewNewUser)
+        myTextView.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
